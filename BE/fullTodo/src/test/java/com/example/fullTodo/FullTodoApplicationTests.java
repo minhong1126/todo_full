3009cbe1cb1e25488fe8344f5460cb2136c1ca3d
minhong1126 <mjhong159357@gmail.com>
2025-02-03 17:20:41 +0900

    BE-spring, FE-vite(Vue) 환경설정

---
+++ BE/fullTodo/src/test/java/com/example/fullTodo/FullTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
